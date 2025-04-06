905d3e19f6639f93cf5e06fa98832f7b359c4624
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-04-06 15:05:59 +0600

    create post input section and store the input section data in database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default async function Home() {
+// import { CreatePost } from "@/components/CreatePost";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
   return (
-    <div className="space-x-5">
-      <h1>Home page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        Who to follow section
+      </div>
     </div>
   );
 }
