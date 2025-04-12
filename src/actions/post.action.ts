dc1a526728b8f119893f7e8b9b1ce97e2801ad49
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-04-12 15:15:56 +0600

    create commenting handleDelet action in button own user

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -31,6 +31,7 @@ export async function getAllPost() {
       include: {
         author: {
           select: {
+            id: true,
             name: true,
             image: true,
             username: true,
@@ -127,5 +128,79 @@ export async function toggleLike(postId: string) {
   } catch (error) {
     console.log("error in the failed toggle likes", error);
     return { success: false, error: "Failed to likes" };
+  }
+}
+export async function createComment(postId: string, content: string) {
+  try {
+    const userId = await getDbUserId();
+    if (!userId) {
+      return;
+    }
+    if (!content) {
+      throw new Error("Content must be required");
+    }
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+    if (!post) {
+      throw new Error("Your post not found at this time");
+    }
+
+    const [comment] = await prisma.$transaction(async (tx) => {
+      // create comment when a user write anything in the comment box area
+      const newComment = await tx.comment.create({
+        data: {
+          content,
+          authorId: userId,
+          postId,
+        },
+      });
+
+      // create notification for if a user commenting on someone others post
+      if (post.authorId !== userId) {
+        await tx.notification.create({
+          data: {
+            type: "COMMENT",
+            userId: post.authorId,
+            creatorId: userId,
+            postId,
+            commentId: newComment.id,
+          },
+        });
+      }
+      return [newComment];
+    });
+    revalidatePath("/");
+    return { success: true, comment };
+  } catch (error) {
+    console.log("Failed to create comment ", error);
+    return { success: false, error: "Opps! comment failed" };
+  }
+}
+export async function deletePost(postId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) {
+      throw new Error("Your post not found at this momment");
+    }
+    if (post.authorId !== userId) {
+      throw new Error("Oppss! you don't have permission for delete this post");
+    }
+
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.log("Faild to deleted post from Deleted post", error);
+    return { success: false, error: "Faild to deleted post from Deleted post" };
   }
 }
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { getAllPost } from "@/actions/post.action";
+import {
+  createComment,
+  deletePost,
+  getAllPost,
+  toggleLike,
+} from "@/actions/post.action";
 import { useUser } from "@clerk/nextjs";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Card, CardContent } from "./ui/card";
+import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+import { formatDistanceToNow } from "date-fns";
+import DeleteAlert from "./DeleteAlert";
+
 type Posts = Awaited<ReturnType<typeof getAllPost>>;
 type Post = Posts[number];
 
@@ -35,10 +48,88 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
     }
   };
 
-  const handleAddComment = async () => {};
+  const handleAddComment = async () => {
+    if (!newComment.trim() || isCommenting) {
+      return;
+    }
 
-  const handleDeletePost = async () => {};
-  return <div>PostCard</div>;
+    try {
+      setIsCommenting(true);
+      const result = await createComment(post.id, newComment);
+      if (result?.success) {
+        toast.success("Your comment done successfully!");
+        setNewComment("");
+      }
+    } catch (error) {
+      toast.error("Opps! failed your comment");
+      setNewComment("");
+    } finally {
+      setIsCommenting(false);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    if (isDeleting) {
+      return;
+    }
+    try {
+      setIsDeleting(true);
+      const result = await deletePost(post.id);
+      if (result?.success) {
+        toast.success("Remove your post");
+      } else {
+        throw new Error(result.error);
+      }
+    } catch (error) {
+      toast.error("Opps! failed to deleted post");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+  return (
+    <Card className="overflow-hidden ">
+      <CardContent className="p-4 sm:p-6">
+        <div className="space-y-4">
+          {/* Post user Image */}
+          <div className="flex gap-3 sm:space-x-4">
+            <Link href={`/profile/${post.author.username}`}>
+              <Avatar className="size-9 sm:w-10 sm:h-10">
+                <AvatarImage src={post.author.image ?? "/avatar.png"} />
+              </Avatar>
+            </Link>
+            {/* post and commenting box */}
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-2 truncate">
+                  <Link
+                    href={`/profile/${post.author.username}`}
+                    className="font-semibold truncate"
+                  >
+                    {post.author.name}
+                  </Link>
+                  <div className="flex items-center space-x-2 text-sm text-muted-foreground">
+                    <Link href={`/profile/${post.author.username}`}>
+                      @{post.author.username}
+                    </Link>
+                    <span>.</span>
+                    <span>
+                      {formatDistanceToNow(new Date(post.createdAt))} ago
+                    </span>
+                  </div>
+                </div>
+                {dbUserId === post.author.id && (
+                  <DeleteAlert
+                    isDeleting={isDeleting}
+                    onDelete={handleDeletePost}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default PostCard;
